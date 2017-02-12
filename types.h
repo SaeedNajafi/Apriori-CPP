e9d752df025c08827e12317bc86bcf8a837d5486
Saeed Najafi <snajafi@ualberta.ca>
2017-02-12 01:10:08 -0700

    data structure added

---
+++ types.h
@@ -0,0 +1,25 @@
+#ifndef TYPES_H	
+#define TYPES_H
+
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <vector>
+
+typedef struct{
+	std::unordered_set<int> items;
+    float r;
+    float cosine_of_theta;
+}itemset;
+
+typedef struct{
+	std::unordered_map<string,itemset> itemsets;
+	std::unordered_map<string,int> counts;
+	int k;
+}lk;
+
+typedef struct{
+	std::vector<lk> lks;
+}lkArray;
+
+#endif
